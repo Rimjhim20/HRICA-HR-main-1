136d2abca34f120e40875bb7ff64c86b82b2b93a
Shivangi Dubey <shivangi942@users.noreply.github.com>
2022-04-23 00:37:31 +0530

    About code in react

---
+++ src/components/About.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "../Style/style.css";
+function About(props) {
+  return (
+    <div class="container">
+      <div class="bgImg img1">
+        <div class="Text" id="about1">
+          <p class="text1">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Repudiandae expedita vero autem nihil commodi doloremque repellendus
+            adipisci nulla nobis eum impedit aliquid, accusamus laudantium
+            quibusdam officia? Nisi quod sit fuga! Lorem ipsum, dolor sit amet
+            consectetur adipisicing elit. Iusto velit cupiditate tenetur ad
+            omnis quam sequi temporibus amet. Praesentium illo odit error, nobis
+            ratione possimus excepturi similique quidem voluptatem
+            exercitationem!
+          </p>
+        </div>
+
+        <div class="Text" id="about2">
+          <p class="text2">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio
+            dicta possimus explicabo sed eveniet quia eos deleniti maiores,
+            voluptates beatae provident rerum iusto laboriosam sequi ad id
+            perferendis quisquam laudantium. Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Sunt illo error, autem doloremque
+            tempore debitis esse id eius iste saepe excepturi asperiores est,
+            reiciendis quas culpa facilis neque provident ab?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
